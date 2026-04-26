aa1b1fd51a63fad8f971c6a7336e54d89c5d85be
TruongDuyKhai <truongduykhai1012@gmail.com>
2026-04-26 16:52:16 +0000

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -52,6 +52,7 @@ export default function App() {
                     <Route path="*" element={<Navigate to="/dashboard" replace />} />
                 </Routes>
             </BrowserRouter>
+            </DataProvider>
         </AuthProvider>
     );
 }
